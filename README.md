e07c806957d2fc83a1d0564806bce993d3ab97a5
Inayat Fatima Qureshi <inayat.fatima.qureshi@gmail.com>
2025-10-19 21:24:33 +0800

    started working on header

--- README.md
+++ README.md
@@ -2,12 +2,7 @@
 A demo version of the potential structure, layout, and animations that can be used to build an ecommerce store.
 
 ## Features
-- Charts to visualize updates
-- Product list for advertisement
-- Tables with real-time updated with backend connectivity
-- Side-bar dashboard for optimization and efficiency
-- Other social media platforms for connectivity
-- Order system for delivery or pick up
+- showcase of services
 
 ## Tech Stack
 - **Frontend:** React, Vite, Lucide
@@ -18,9 +13,7 @@ A demo version of the potential structure, layout, and animations that can be us
 [Live Demo will be available here once dashboard is completed](link_here)
 
 ## Development Notes
-- Initialized project with Vite, installed Tailwind CSS and Rechart
+- Initialized project with Vite, installed Tailwind CSS and Lucide
 - Configured Git repository & basic setup
-- Implemented core functionality using useEffect, useState, and useRef
-- Implemented dynamic sidebar, header with user profile, and a stats grid
 - Styled the application with custom CSS
 - Published via GitHub Pages
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import { Sparkle } from 'lucide-react'
+import React from 'react'
+
+function Header() {
+  return (
+    <header className={`fixed top-0 left-0 right-0 z-50 transition-all duration`}>
+        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+            <div className='flex justify-between items-center py-4'>
+                <div className='relative'>
+                    <Sparkle className={`w-8 h-8 transition-all duration-300`}/>
+                    {/* <div 
+                        className='
+                            absolute -top-1 -right-1 w-3 h-3 bg-gradient-to-r from-pink-500 to-purple-500 rounded-full animation-pulse
+                        '
+                    ></div> */}
+                    <h1 
+                        className={`
+                            text-2xl font-black bg-gradient-to-r from-purple-600 to-pink-600 bg-clip-text transition-all duration-300
+                        `}
+                    >
+                        Nexus
+                    </h1>
+                </div>                
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
